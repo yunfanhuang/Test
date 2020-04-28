4d5d4c97ad34eed807aba2d11b5d74ff22e4bb6c
H_helen <w94njo4bobo@gmail.com>
2020-04-28 23:09:41 +0800

    new commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEntryPoint.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appsdeveloperblog.app.ws.ui.entrypoints;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author BOBO
+ */
+@Path("/users")
+public class UsersEntryPoint {
+    
+    @POST
+    public void createUser(){
+    
+    }
+    
+}
